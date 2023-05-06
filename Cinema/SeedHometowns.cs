35190ab8ad8e0ee380e3a1266eabd4e81989c0ee
Filippo Ferrario <filippo.ferrario@issgreppi.it>
2023-05-06 09:25:13 +0200

    Update Github actions

--- Cinema/SeedHometowns.cs
+++ Cinema/SeedHometowns.cs
@@ -8,7 +8,7 @@ namespace Cinema
 	{
 		public static void Initialize(IServiceProvider provider)
 		{
-			using var unitOfWork = new CinemaDbContext(
+			var unitOfWork = new CinemaDbContext(
 				provider.GetRequiredService<DbContextOptions<CinemaDbContext>>());
 
 			if (unitOfWork is null || unitOfWork.Towns is null || unitOfWork.Towns.Any())
